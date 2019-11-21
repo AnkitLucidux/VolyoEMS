941acf8ac22719a63a203792780eba6d215763d1
Vaibhav Jain <VaibhavJ@lucidux.com>
2019-11-21 17:06:34 +0530

    Holiday Controller and model.

---
+++ EMS.Entities/Entities/Holiday.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EMS.Entities
+{
+    public class Holiday
+    {
+        [Key]
+        public int HolidayId { get; set; }
+
+        [Required(ErrorMessage = "Holiday date required.")]
+        [DisplayName("Holiday date")]
+        public DateTime HolidayDate { get; set; }
+
+        [Required(ErrorMessage = "Holiday name required.")]
+        [StringLength(50, ErrorMessage = "The field {0} must be a maximum length of '50'.")]
+        [DisplayName("Holiday Name")]
+        public string HolidayName { get; set; }
+    }
+}
---
+++ EMS.Web/Controllers/HolidayController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EMS.Entities;
+using EMS.Web.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Web.Controllers
+{
+    public class HolidayController : Controller
+    {
+        private readonly AdminRepository _adminRepository;
+        public HolidayController(AdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_adminRepository.GetAllHolidayList());
+        }
+
+        // GET: Holiday/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Holiday/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Holiday holidayModel)
+        {
+            try
+            {
+                var checkHoliday = _adminRepository.GetHolidayByName(holidayModel.HolidayName);
+
+                if (checkHoliday != null)
+                {
+                    this.TempData["ErrorMessage"] = "This holiday is already exists!";
+                    return View(holidayModel);
+                }
+
+                var addHoliday = _adminRepository.AddUpdateHoliday(holidayModel);
+                if (addHoliday != null)
+                {
+                    this.TempData["SuccessMessage"] = "Holiday added Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                this.TempData["ErrorMessage"] = "Something Went Wrong. Please try again!";
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Holiday/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Holiday/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Holiday/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Holiday/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
